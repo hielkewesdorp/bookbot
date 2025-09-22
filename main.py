863bbf921865198791cf4261fbb25439915e1878
hielkewesdorp <hielkewesdorp@gmail.com>
2025-09-22 20:58:47 +0200

    added command line control of which book

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #print("hello")
+import sys
 from stats import get_book_text
 from stats import word_counter
 from stats import character_counter
 from stats import character_sorter
-path = "books/frankenstein.txt"
+#path = "books/frankenstein.txt"
 
-
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+path = sys.argv[1]
 
 
 
