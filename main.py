fd5bdf8a104028751375822631653638c4cc97d9
hielkewesdorp <hielkewesdorp@gmail.com>
2025-09-22 16:20:34 +0200

    read book file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+#print("hello")
+def get_book_text(book_filepath):
+    with open(book_filepath) as file:
+        book_as_string = file.read()
+    return book_as_string
+
+path = "books/frankenstein.txt"
+
+def main():
+    text1 = get_book_text(path)
+    print(text1)
+
+main()
