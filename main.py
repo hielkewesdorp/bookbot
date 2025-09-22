914f5844b186a2e79afb7887ad8095093d4a792a
hielkewesdorp <hielkewesdorp@gmail.com>
2025-09-22 16:30:47 +0200

    added wordcounter

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ def get_book_text(book_filepath):
     return book_as_string
 
 path = "books/frankenstein.txt"
-
+wordlist = []
+wordcount = 0
+def word_counter(book_filepath):
+    text1 = get_book_text(book_filepath)
+    wordlist = text1.split()
+    #print(wordlist)
+    wordcount = len(wordlist)
+    return wordcount
 def main():
-    text1 = get_book_text(path)
-    print(text1)
+    print(f"{word_counter(path)} words found in the document")
+    #text1 = get_book_text(path)
+    #print(text1)
+
 
 main()
